064e70468e9186d9ed45bcd8796bf5b8ea76dbe1
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2016-03-25 18:02:13 +0200

    some simple app loading util implementation

--- app/src/main/java/io/wumf/wumf/util/AppUtils.java
+++ app/src/main/java/io/wumf/wumf/util/AppUtils.java
@@ -1,7 +1,12 @@
 package io.wumf.wumf.util;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.wumf.wumf.memory.App;
@@ -18,11 +23,46 @@ public class AppUtils {
     }
 
     public List<App> loadAllAppsFromSystem() {
-        return null;
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfos = getResolveInfos(pm);
+        return resolveInfoToApp(pm, resolveInfos);
     }
 
     public App loadAppFromSystem(String packageName) {
         return null;
+    }
+
+    private App resolveInfoToApp(PackageManager pm, ResolveInfo resolveInfo) {
+        App app = new App();
+        app.setInstallDate(getInstallDate(pm, resolveInfo.activityInfo.packageName));
+        app.setPackageName(resolveInfo.activityInfo.packageName);
+        app.setRemoved(false);
+        return app;
+    }
+
+    private List<App> resolveInfoToApp(PackageManager pm, List<ResolveInfo> list) {
+        List<App> result = new ArrayList<>();
+        for (ResolveInfo resolveInfo : list) {
+            result.add(resolveInfoToApp(pm, resolveInfo));
+        }
+        return result;
+    }
+
+    private List<ResolveInfo> getResolveInfos(PackageManager pm) {
+        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final List<android.content.pm.ResolveInfo> appList = pm.queryIntentActivities(mainIntent, PackageManager.GET_META_DATA);
+        return appList;
+    }
+
+    private long getInstallDate(PackageManager pm, String packageName) {
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, 0);
+            return packageInfo.firstInstallTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
 }
