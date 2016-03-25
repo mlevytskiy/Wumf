af562d8495cfe4c9d7f867e1c8f031cedb8c19ce
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2016-03-25 09:01:21 +0200

    little refactoring

---
+++ app/src/main/java/io/wumf/wumf/DummyFragment.java
@@ -0,0 +1,54 @@
+package io.wumf.wumf;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by max on 25.03.16.
+ */
+public class DummyFragment extends Fragment {
+    int color;
+    SimpleRecyclerAdapter adapter;
+
+    public DummyFragment() {
+    }
+
+    @SuppressLint("ValidFragment")
+    public DummyFragment(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dummy_fragment, container, false);
+
+        final View frameLayout = view.findViewById(R.id.dummyfrag_bg);
+        frameLayout.setBackgroundColor(color);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.dummyfrag_scrollableview);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < VersionModel.data.length; i++) {
+            list.add(VersionModel.data[i]);
+        }
+
+        adapter = new SimpleRecyclerAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+}
--- app/src/main/java/io/wumf/wumf/MainActivity.java
+++ app/src/main/java/io/wumf/wumf/MainActivity.java
@@ -1,6 +1,5 @@
 package io.wumf.wumf;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -10,12 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -126,40 +120,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static class DummyFragment extends Fragment {
-        int color;
-        SimpleRecyclerAdapter adapter;
-
-        public DummyFragment() {
-        }
-
-        @SuppressLint("ValidFragment")
-        public DummyFragment(int color) {
-            this.color = color;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.dummy_fragment, container, false);
-
-            final View frameLayout = view.findViewById(R.id.dummyfrag_bg);
-            frameLayout.setBackgroundColor(color);
-
-            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.dummyfrag_scrollableview);
-
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
-            recyclerView.setLayoutManager(linearLayoutManager);
-            recyclerView.setHasFixedSize(true);
-
-            List<String> list = new ArrayList<String>();
-            for (int i = 0; i < VersionModel.data.length; i++) {
-                list.add(VersionModel.data[i]);
-            }
-
-            adapter = new SimpleRecyclerAdapter(list);
-            recyclerView.setAdapter(adapter);
-
-            return view;
-        }
-    }
 }
