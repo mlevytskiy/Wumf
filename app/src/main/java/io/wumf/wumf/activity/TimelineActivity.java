b029a694b7f820443e026f0d6d3e30d59c15c10c
Oleg Green <olegalex.green@gmail.com>
2016-05-01 01:00:16 +0300

    adding navigation menu to project and small improvements in gradle config

--- app/src/main/java/io/wumf/wumf/activity/TimelineActivity.java
+++ app/src/main/java/io/wumf/wumf/activity/TimelineActivity.java
@@ -1,15 +1,24 @@
 package io.wumf.wumf.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.squareup.otto.Subscribe;
 
@@ -22,20 +31,30 @@ import io.wumf.wumf.fragment.TimelineFragment;
 import io.wumf.wumf.otto.BusProvider;
 import io.wumf.wumf.otto.event.OnAppItemClickEvent;
 
-public class TimelineActivity extends PrepareDataActivity {
+public class TimelineActivity extends PrepareDataActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarLayout appBarLayout;
     private TabLayout tabLayout;
+    private DrawerLayout mDrawerLayout;
+    private AppDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreateAfterDataPreparation(Bundle savedInstanceState) {
         setContentView(R.layout.activity_tab_animation);
 
         appBarLayout = (AppBarLayout) findViewById(R.id.tabanim_appbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.tabanim_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        ActionBar supportActionBar = getSupportActionBar();
+
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeButtonEnabled(true);
+        }
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.tabanim_viewpager);
         setupViewPager(viewPager);
@@ -83,6 +102,56 @@ public class TimelineActivity extends PrepareDataActivity {
             }
         });
 
+        mDrawerToggle = new AppDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_item_1:
+                Toast.makeText(TimelineActivity.this, "Item 1", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.navigation_item_2:
+                Toast.makeText(TimelineActivity.this, "Item 2", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.navigation_item_3:
+                Toast.makeText(TimelineActivity.this, "Item 3", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.navigation_item_4:
+                Toast.makeText(TimelineActivity.this, "Item 4", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mDrawerLayout.closeDrawers();
+        return true;
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -136,4 +205,26 @@ public class TimelineActivity extends PrepareDataActivity {
         BusProvider.getInstance().unregister(this);
     }
 
+
+    private static class AppDrawerToggle extends ActionBarDrawerToggle {
+        private final AppCompatActivity mActivity;
+
+        public AppDrawerToggle(AppCompatActivity mainActivity, DrawerLayout drawerLayout,
+                               int navigation_drawer_open, int navigation_drawer_close) {
+            super(mainActivity, drawerLayout, navigation_drawer_open, navigation_drawer_close);
+            mActivity = mainActivity;
+        }
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            super.onDrawerClosed(drawerView);
+            mActivity.supportInvalidateOptionsMenu();
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            mActivity.supportInvalidateOptionsMenu();
+        }
+    }
 }
