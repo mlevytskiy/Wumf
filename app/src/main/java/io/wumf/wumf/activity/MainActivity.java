568d01a28f014b5eb6866230c331c5c587635bd0
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2016-05-09 18:35:54 +0300

    remove side menu

--- app/src/main/java/io/wumf/wumf/activity/MainActivity.java
+++ app/src/main/java/io/wumf/wumf/activity/MainActivity.java
@@ -1,11 +1,9 @@
 package io.wumf.wumf.activity;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,9 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.squareup.otto.Subscribe;
 
@@ -31,21 +27,16 @@ import io.wumf.wumf.fragment.TimelineFragment;
 import io.wumf.wumf.otto.BusProvider;
 import io.wumf.wumf.otto.event.OnAppItemClickEvent;
 
-public class MainActivity extends PrepareDataActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends PrepareDataActivity {
 
     private AppBarLayout appBarLayout;
     private TabLayout tabLayout;
-    private DrawerLayout mDrawerLayout;
-    private AppDrawerToggle mDrawerToggle;
-    private NavigationView mNavigationView;
 
     @Override
     protected void onCreateAfterDataPreparation(Bundle savedInstanceState) {
         setContentView(R.layout.activity_tab_animation);
 
         appBarLayout = (AppBarLayout) findViewById(R.id.tabanim_appbar);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.tabanim_toolbar);
         setSupportActionBar(toolbar);
@@ -96,51 +87,6 @@ public class MainActivity extends PrepareDataActivity implements NavigationView.
 
             }
         });
-
-        mDrawerToggle = new AppDrawerToggle(
-                this,
-                mDrawerLayout,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        );
-
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
-        mNavigationView.setNavigationItemSelectedListener(this);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.navigation_item_1:
-                Toast.makeText(MainActivity.this, "Item 1", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navigation_item_2:
-                Toast.makeText(MainActivity.this, "Item 2", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navigation_item_3:
-                Toast.makeText(MainActivity.this, "Item 3", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navigation_item_4:
-                Toast.makeText(MainActivity.this, "Item 4", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        mDrawerLayout.closeDrawers();
-        return true;
     }
 
     private void setupViewPager(ViewPager viewPager) {
